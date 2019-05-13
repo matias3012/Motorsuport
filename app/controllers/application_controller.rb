12ace505338fd6d2cdf0c113c22c080a904230c0
matias3012 <juankstack@gmail.com>
2019-05-13 16:14:03 -0500

    cambio de presentacion en index de email por nombre

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+before_action :name_devise_param, if: :devise_controller?
+
+  def name_devise_param
+    devise_parameter_sanitizer.permit(:sign_up) do |rider|
+      rider.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+
 end
