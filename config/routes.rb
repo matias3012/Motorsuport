50e0465e56459b37624720954f979d82e64e7d4f
matias3012 <juankstack@gmail.com>
2019-05-13 15:52:05 -0500

    gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :riders
   root to: 'blogs#index'
   resources :comments
   resources :blogs
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_223132) do
+ActiveRecord::Schema.define(version: 2019_05_13_144542) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,19 @@ ActiveRecord::Schema.define(version: 2019_05_08_223132) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["motorsuport_id"], name: "index_comments_on_motorsuport_id"
+  end
+
+  create_table "riders", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_riders_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_riders_on_reset_password_token", unique: true
   end
 
 end
