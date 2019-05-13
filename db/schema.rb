c4f97bf39647281f2ef46bfda1d8b1b0af075faf
matias3012 <juankstack@gmail.com>
2019-05-13 09:15:55 -0500

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_213220) do
+ActiveRecord::Schema.define(version: 2019_05_08_223132) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_213220) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
